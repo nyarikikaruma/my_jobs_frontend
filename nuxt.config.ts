8a43bcb9c424d65dc98063faf513f0bd54b77338
nyarikikaruma <nyarikikaruma@gmail.com>
2025-09-03 07:21:26 +0300

    Sitemap added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@pinia/nuxt',
-    'nuxt-simple-sitemap'
-  ],
-  sitemap: {
-    siteUrl: 'https://my-jobs-frontend.vercel.app',
-  },
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxtjs/sitemap'],
    nitro: {
     devProxy: {
       '/api': {
